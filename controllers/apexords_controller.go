2f5bc1320a938948133636dc3203a6ba82915efd
HenryXie1 <xiehonghuicn@gmail.com>
2019-07-27 17:16:09 +1000

    update yaml

--- controllers/apexords_controller.go
+++ controllers/apexords_controller.go
@@ -24,8 +24,8 @@ import (
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	appsv1beta1 "k8s.io/api/apps/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
